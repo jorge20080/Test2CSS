4d8911719364beb2712c287ed230eb946dca3195
jorge20080 <122296192+jorge20080@users.noreply.github.com>
2023-12-15 16:53:06 -0500

    Add files via upload

---
+++ final_Jorge_0826009/swiper.js
@@ -0,0 +1,8 @@
+let mySwiper = new Swiper('.swiper-container', {
+    direction: 'horizontal', 
+    loop: true,
+    navigation: {          
+        nextEl: ".swiper-button-next",          
+        prevEl: ".swiper-button-prev",        
+    }
+});
